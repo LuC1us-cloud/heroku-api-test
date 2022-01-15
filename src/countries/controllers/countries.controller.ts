293621b5f495906c76d07c30724a604cb20d37b1
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-01-15 21:46:05 +0200

    First endpoint completed. Stopped tracking env file

--- src/countries/controllers/countries.controller.ts
+++ src/countries/controllers/countries.controller.ts
@@ -3,17 +3,29 @@ import { Observable } from 'rxjs';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { Country } from '../models/country.interface';
 import { CountriesService } from '../services/countries.service';
-
+import fetch from 'node-fetch';
 @Controller('countries')
 export class CountriesController {
     constructor(private countriesService: CountriesService) {}
 
     @Get('list')
-    availableCountriesList(@Body() country: Country): Observable<Country> {
-        return this.countriesService.createCountry(country);
+    // Not gonna make this method save country info, because the list could change on the API provider side 
+    // and there would be no way of without calling the same API again.
+    // tldr: making this method save responses from API would not save any time.
+    async availableCountriesList(): Promise<Observable<Country>> {
+        const response = await fetch('https://kayaposoft.com/enrico/json/v2.0/?action=getSupportedCountries');
+        const json = await response.json();
+        // iterate over the array of countries and return each countries fullName and countryCode
+        const countries = json.map(country => {
+            return {
+                fullName: country.fullName,
+                countryCode: country.countryCode,
+            };
+        });
+        return countries;
     }
 
-    @Get('grouped')
+    @Get('grouped/:country/:year')
     groupedByMonthHolidayListForCountry(): Observable<Country[]> {
         return this.countriesService.findAll();
     }
