be60c72d3452b8d81ac4e57b9c2b28029e8aae2d
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-01-15 19:14:24 +0200

    Added files for heroku

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 console.log('Server running on port 3000');
 bootstrap();
